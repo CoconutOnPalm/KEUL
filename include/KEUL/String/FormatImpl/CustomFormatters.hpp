8d4691dc59f9c885f2c3599d78cb56319ba9f29e
CoconutOnPalm <adam.kostrzewski@hotmail.com>
2025-06-30 12:01:28 +0200

    Changed CustomForrmatters into simplified versions

--- include/KEUL/String/FormatImpl/CustomFormatters.hpp
+++ include/KEUL/String/FormatImpl/CustomFormatters.hpp
@@ -4,79 +4,60 @@
 #include <vector>
 #include <array>
 
-// TODO: Remove this when range formatting is supported
 
-// std::vector<T> formatter (until range formatting is supported)
-
+// clang already has them defined
 #ifndef __clang__
 
-template <typename T, typename CharT>
-struct std::formatter<std::vector<T>, CharT>
-{
-	constexpr auto parse(std::basic_format_parse_context<CharT>& ctx)
-	{
-		return ctx.begin();
-	}
 
-	template <typename FormatContext>
-	auto format(const std::vector<T>& range, FormatContext& ctx) const
+// std::vector
+template <typename T>
+struct std::formatter<std::vector<T>> : std::formatter<std::string>
+{
+	auto format(const std::vector<T>& vector, std::format_context& ctx) const
 	{
 		std::string elements;
-		for (const auto& itr : range)
+		for (const auto& itr : vector)
 			elements += std::format("{}, ", itr);
 
-		if (range.size() > 0)
+		if (vector.size() > 0)
 		{
 			elements.pop_back();
 			elements.pop_back();
 		}
 
-		return std::format_to(ctx.out(), "[{}]", elements);
+		return std::formatter<std::string>::format(std::format("[{}]", elements), ctx);
 	}
 };
 
 
-// std::array<T, _Size> formatter (until range formatting is supported)
-template <typename T, size_t _Size, typename CharT>
-struct std::formatter<std::array<T, _Size>, CharT>
+// std::array
+template <typename T, size_t _size>
+struct std::formatter<std::array<T, _size>> : std::formatter<std::string>
 {
-	constexpr auto parse(std::basic_format_parse_context<CharT>& ctx)
-	{
-		return ctx.begin();
-	}
-
-	template <typename FormatContext>
-	auto format(const std::array<T, _Size>& range, FormatContext& ctx) const
+	auto format(const std::array<T, _size>& array, std::format_context& ctx) const
 	{
 		std::string elements;
-		for (const auto& itr : range)
+		for (const auto& itr : array)
 			elements += std::format("{}, ", itr);
 
-		if (range.size() > 0)
+		if (array.size() > 0)
 		{
 			elements.pop_back();
 			elements.pop_back();
 		}
 
-		return std::format_to(ctx.out(), "[{}]", elements);
+		return std::formatter<std::string>::format(std::format("[{}]", elements), ctx);
 	}
 };
 
 
-// std::pair<T1, T2> formatter (until range formatting is supported)
-template <typename T1, typename T2,  typename CharT>
-struct std::formatter<std::pair<T1, T2>, CharT>
+// std::pair
+template <typename T1, typename T2>
+struct std::formatter<std::pair<T1, T2>> : std::formatter<std::string>
 {
-	constexpr auto parse(std::basic_format_parse_context<CharT>& ctx)
+	auto format(const std::pair<T1, T2>& pair, std::format_context& ctx) const
 	{
-		return ctx.begin();
-	}
-
-	template <typename FormatContext>
-	auto format(const std::pair<T1, T2>& pair, FormatContext& ctx) const
-	{
-		std::string elements = std::format("{}, {}", pair.first, pair.second);
-		return std::format_to(ctx.out(), "({})", elements);
+		return std::formatter<std::string>::format(std::format("({}, {})", pair.first, pair.second), ctx);
 	}
 };
 
